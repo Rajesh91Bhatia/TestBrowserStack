bc0f0205b54be41b59c612bdd9d15309aadda7fe
Node <Node@DESKTOP-FEJ2M74>
2020-04-29 17:24:44 +0530

    TestPipeLine Update

--- TestProject_WithBrowserStack/src/test/java/TestNG_WithBS.java
+++ TestProject_WithBrowserStack/src/test/java/TestNG_WithBS.java
@@ -1,5 +1,13 @@
 import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.Parameters;
+
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -9,22 +17,23 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+
 
 public class TestNG_WithBS {
 	public static final String USERNAME = "rajeshbhatia2";
     public static final String AUTOMATE_KEY = "pAbNdxjwZZkZ5aC9vEKg";
 	public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
 
-	
-  @Test
-  public void TestGoogleLauchWithBS() throws MalformedURLException {
+	@Test(enabled=false)
+    public void TestGoogleLauchWithBS(String test_browser,String Version, String OS_name, String OS_Version) throws MalformedURLException {
 	  DesiredCapabilities caps = new DesiredCapabilities();
-	    caps.setCapability("browser", "Chrome");
-	    caps.setCapability("browser_version", "81.0");
-	    caps.setCapability("os", "Windows");
-	    caps.setCapability("os_version", "10");
+	    caps.setCapability("browser", test_browser);
+	    caps.setCapability("browser_version", Version);
+	    caps.setCapability("os", OS_name);
+	    caps.setCapability("os_version", OS_Version);
 	    caps.setCapability("resolution", "1024x768");
-	    caps.setCapability("name", "Bstack-[Java] Sample Test");
+	    caps.setCapability("name", test_browser + "test");
 
 	    WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
 	    driver.get("http://www.google.com");
@@ -37,6 +46,39 @@ public class TestNG_WithBS {
 	    driver.quit();
 
   }
+	
+	@Test
+	public void MobileTest() throws MalformedURLException{
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability("os_version", "11");
+		caps.setCapability("device", "iPhone 8 Plus");
+		caps.setCapability("real_mobile", "true");
+		caps.setCapability("browser", "Safari");
+	    caps.setCapability("browser_version", "13.0");
+	    caps.setCapability("name", "MobileTest");
+	    
+		WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+	    driver.get("http://www.google.com");
+	    WebElement element = driver.findElement(By.name("q"));
+
+	    element.sendKeys("BrowserStack");
+	    element.submit();
+
+	    System.out.println(driver.getTitle());
+	    driver.quit();
+	}
+	
+	@DataProvider(name="BrowserProvider")
+    public Object[][] getDataFromDataprovider(){
+    return new Object[][] 
+    	{
+            { "Chrome", "81.0","Windows","10"},
+            { "IE", "11","Windows","10"},
+            { "Firefox", "75","Windows","10"},
+            { "Safari", "13.0","OS X","Catalina"}
+            
+        };
+	};
   
   @BeforeMethod
   public void beforeMethod() {
